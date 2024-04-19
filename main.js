5f33da8d95cb9470c16d5c193b0945973a703ec7
Rishard C <rishard.c@hotmail.com>
2024-04-18 20:06:57 -0400

    Add function to handle paddle movement based on user input

---
+++ main.js
@@ -0,0 +1,21 @@
+const paddle1 = document.querySelector('.box1');
+const paddle2 = document.querySelector('.box2');
+
+const paddleMove = () => {
+  document.addEventListener('keydown', (event) => {
+    // Check which keys are pressed
+    if (event.key === 'w') {
+      // Move paddle1 up
+      
+    } else if (event.key === 's') {
+      // Move paddle1 down
+      
+    } else if (event.key === 'ArrowUp') {
+      // Move paddle2 up
+      
+    } else if (event.key === 'ArrowDown') {
+      // Move paddle2 down
+      
+    }
+  });
+}
